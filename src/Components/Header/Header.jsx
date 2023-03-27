9ede389a234780a5b47cde05116d251c1d326c47
jawad Bin Azam <abidibnazam@gmail.com>
2023-03-27 15:01:47 +0600

    added media files

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import logo from '../../images/Logo.svg';
+import './Header.css';
 const Header = () => {
     return (
-        <div>
-            <h1>banana</h1>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div className="">
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
