6f16af8056a998d2b21e80a3e8404f5984a97a79
jawad Bin Azam <abidibnazam@gmail.com>
2023-03-28 10:29:32 +0600

    added button

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,12 +3,21 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product)
-    const {img, name, seller, price, quantity} = props.product;
+    const {img, name, seller, ratings, price, quantity} = props.product;
     return (
         <div className='product'>
-            <img src={img} alt="" />
-            <h6>{name}</h6>
-            <p className="">Price:${price}</p>
+            <div className="image-style">
+                <img src={img} alt="" />
+            </div>
+            <div className="product-info">
+                <h6 className='product-name'>{name}</h6>
+                <p className="">Price:${price}</p>
+                <div className="sub-info">
+                    <p>Manufacturer: {seller}</p>
+                    <p>Ratiings: {ratings} Star </p>
+                </div>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
