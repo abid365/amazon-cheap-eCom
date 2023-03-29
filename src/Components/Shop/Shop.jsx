cb5718b8bd5d09a94806689e30809df8ed01181c
jawad Bin Azam <abidibnazam@gmail.com>
2023-03-29 17:46:15 +0600

    save cart to local storage

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,6 +12,11 @@ const Shop = () => {
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
+    }, [])
+
+    useEffect(()=>{
+       const storedCart = getShoppingCart();
+       console.log(storedCart);
     }, [])
 
     const handleAddToCart =(product)=>{
